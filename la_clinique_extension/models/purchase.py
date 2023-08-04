b6d6267e027f466f7d94ab1ce266a4c3cfb15245
Sathish-0901 <sathishkmr12345@gmail.com>
2023-08-04 15:44:14 +0530

    removed tax validation in both so and po API

--- la_clinique_extension/models/purchase.py
+++ la_clinique_extension/models/purchase.py
@@ -1,6 +1,8 @@
 from odoo import api, fields, models, SUPERUSER_ID, _
 import logging
 _logger = logging.getLogger(__name__)
+from odoo.exceptions import AccessError, UserError, ValidationError
+
 
 
 class PurchaseOrder(models.Model):
@@ -14,6 +16,16 @@ class PurchaseOrder(models.Model):
         invoice_vals = super(PurchaseOrder, self)._prepare_invoice()
         invoice_vals['inv_moc_doc_ref'] = self.moc_doc_ref if self.moc_doc_ref else False
         return invoice_vals
+
+    def action_create_invoice(self):
+        for order in self:
+            if order.create_api_values:
+                for line in order.order_line:
+                    if not line.taxes_id:
+                        raise UserError(_('The Tax Is Not Mapped for the Product: %s , Kinldy Map It.' % (line.product_id.name)))
+        values = super(PurchaseOrder, self).action_create_invoice()
+        return values
+
 
 
 
