925e2c6f08780e1ae7771f4ab832e3e64f99ba00
Aarthi Siva <aarthi.siva@apagen.com>
2023-08-08 12:40:31 +0530

    Aarthi - add trackvisibility for insurance company, Agreed amount, paid amount for invoice

--- la_clinique_extension/models/invoice.py
+++ la_clinique_extension/models/invoice.py
@@ -6,9 +6,9 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    inv_insurance_provider_id = fields.Many2one('insurance.provider',string="Insurance Company",copy=False)
-    inv_agreed_amount = fields.Float(string="Agreed Amount",copy=False)
-    inv_actual_paid = fields.Float(string="Actual Paid",copy=False)
+    inv_insurance_provider_id = fields.Many2one('insurance.provider',string="Insurance Company",copy=False, track_visibility="onchange")
+    inv_agreed_amount = fields.Float(string="Agreed Amount",copy=False, track_visibility="onchange")
+    inv_actual_paid = fields.Float(string="Actual Paid",copy=False, track_visibility="onchange")
     inv_moc_doc_ref = fields.Char(string="Moc Doc Ref",copy=False)
 
 class AccountMoveLine(models.Model):
