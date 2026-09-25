b1655a12642b1680a509ee0487e26b1c9f1600e7
Aria-odoo <bh@ariajewellers.in>
2026-09-26 00:39:32 +0530

    doctor detail code has been updated

--- la_clinique_extension/models/doctor_details.py
+++ la_clinique_extension/models/doctor_details.py
@@ -204,7 +204,7 @@ class DoctorDetails(models.Model):
 							'name': i.get('dept'),
 							'patient_name': i.get('name'),
 							'bill_ref': i.get('bill_no'),
-							'doctor': i.get('itemname'),
+							'doctor': i.get('subdept'),
 							'doc_fee': i.get('amt'),
 							'bill_date': bill_date.date(),
 							'api_date': api_date,
